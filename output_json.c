0f6ed2486c8ff3ac6743003042d51b294267687d
Okuda <kyokuheki@gmail.com>
2019-09-24 19:50:38 +0900

    Update output_json.c

--- output_json.c
+++ output_json.c
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include "parson.h"
 
-extern boolean separate_score_flag;
 static int outout_json_flag = 0;
 static JSON_Value *root_value = NULL;
 static JSON_Object *root_object = NULL;
